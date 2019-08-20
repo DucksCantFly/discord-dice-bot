45e57cb316cc375540e8b9163a69c521b724b72a
Cory Nelson <cln4d@mtmail.mtsu.edu>
2019-08-19 23:46:25 -0500

    updating parser

--- js/roll-parser.js
+++ js/roll-parser.js
@@ -1,8 +1,26 @@
 /*
 	Provides functionality for parsing string input for DuckDice bot commands
+	
+	Syntax rules
+		Spaces do not matter
+	
+	
+	
+	
 */
 
 
+function is_digit(c){return /\d/.test(c);}
+function is_operator(c){return /d|<|>|=|<=|>=/.test(c);
+
+
+function tokenize(s){
+	
+	var arr = [];
+	str.replace(/\s+/, "");
+	str = str.split(" ");
+	str.forEach(function (char, id){ if is_digit(char))
+}
 
 
 
