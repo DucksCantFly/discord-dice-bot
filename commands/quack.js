a713cf67a13372efd8f1e44e4ba4345b98a2e94d
Cory Nelson <cln4d@mtmail.mtsu.edu>
2019-08-21 18:57:47 -0500

    Implemented Quacking
    Truly, a glimmer of hope in the darkness.

--- commands/quack.js
+++ commands/quack.js
@@ -9,8 +9,14 @@ module.exports = {
 	name: "quack",
 	description: "quacks",
 	execute(message,args){
+		var output = ":duck: ";
+		var quackNum = rand.rollRange(1,6);
+		var punctuation = [" ",".","!","?"]
 		
-		var output = ":duck:";
+		for(var i = 0; i < quackNum; i++){
+			output += "Quack" + punctuation[rand.rollRange(0,punctuation.length)] + " ";
+		}
+		
 		message.channel.send(output);
 	},
 };
